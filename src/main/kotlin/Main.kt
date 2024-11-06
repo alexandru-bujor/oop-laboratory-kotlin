623b934295727e5ba91e9a3cdb7b67146d4cb00a
Bujor Alexandru <alexandru.bujor@isa.utm.md>
2024-11-06 07:55:45 +0200

    The test files and the menu for the main program

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,33 @@
-package org.example
+package lab1
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import lab1.task1.runTask as runTask1
+import lab1.task2.runTask as runTask2
+import lab1.task3.runTask as runTask3
+import lab1.task4.runTask as runTask4
+import java.util.Scanner
+
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val scanner = Scanner(System.`in`)
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    while (true) {
+        println("=== OOP Laboratory Tasks Menu ===")
+        println("1. Task 1: Objects & Object Interaction")
+        println("2. Task 2: Program Arguments and Text Parsing")
+        println("3. Task 3: Class Composition")
+        println("4. Task 4: Bonus Task - Process Multiple Files")
+        println("0. Exit")
+        print("Select a task to run (0 to exit): ")
+
+        when (scanner.nextInt()) {
+            1 -> runTask1()
+            2 -> runTask2()
+            3 -> runTask3()
+            4 -> runTask4()
+            0 -> {
+                println("Exiting the program.")
+                return
+            }
+            else -> println("Invalid option, please try again.")
+        }
     }
 }

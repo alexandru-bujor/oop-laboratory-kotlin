9357de1a6840c4bbdb4eed7f0afe32773c7ab86e
Bujor Alexandru <alexandru.bujor@isa.utm.md>
2024-11-06 07:54:08 +0200

    Task 1: Objects & Object Interaction

--- src/main/kotlin/Display.kt
+++ src/main/kotlin/Display.kt
@@ -1,26 +1,21 @@
-// File: Display.kt
-class Display(
-    private val width: Int,
-    private val height: Int,
-    private val ppi: Float,
-    private val model: String
-) {
+package lab1.task1
 
+class Display(val width: Int, val height: Int, val ppi: Float, val model: String) {
     fun compareSize(other: Display) {
-        val thisSize = width * height
-        val otherSize = other.width * other.height
+        val areaThis = width * height
+        val areaOther = other.width * other.height
         when {
-            thisSize > otherSize -> println("$model has a larger size than ${other.model}")
-            thisSize < otherSize -> println("${other.model} has a larger size than $model")
-            else -> println("$model and ${other.model} have the same size.")
+            areaThis > areaOther -> println("$model has a larger screen size than ${other.model}.")
+            areaThis < areaOther -> println("${other.model} has a larger screen size than $model.")
+            else -> println("$model and ${other.model} have the same screen size.")
         }
     }
 
     fun compareSharpness(other: Display) {
         when {
-            ppi > other.ppi -> println("$model is sharper than ${other.model}")
-            ppi < other.ppi -> println("${other.model} is sharper than $model")
-            else -> println("$model and ${other.model} have the same sharpness.")
+            ppi > other.ppi -> println("$model has a sharper display than ${other.model}.")
+            ppi < other.ppi -> println("${other.model} has a sharper display than $model.")
+            else -> println("$model and ${other.model} have the same display sharpness.")
         }
     }
 
@@ -28,15 +23,20 @@ class Display(
         compareSize(other)
         compareSharpness(other)
     }
+
+
 }
 
-// Main for Task 1
 fun main() {
     val display1 = Display(1920, 1080, 401f, "Display1")
     val display2 = Display(2560, 1440, 530f, "Display2")
     val display3 = Display(3840, 2160, 300f, "Display3")
 
     display1.compareWithMonitor(display2)
-    display2.compareWithMonitor(display3)
     display1.compareWithMonitor(display3)
+    display2.compareWithMonitor(display3)
+}
+
+fun runTask() {
+   main()
 }

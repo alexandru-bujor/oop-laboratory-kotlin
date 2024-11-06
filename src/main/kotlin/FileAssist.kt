64b274ba62a99eae04c7948d8ab7ec6ad01abc92
Bujor Alexandru <alexandru.bujor@isa.utm.md>
2024-11-06 07:55:00 +0200

    Task 3: Class Composition

--- src/main/kotlin/FileAssist.kt
+++ src/main/kotlin/FileAssist.kt
@@ -1,5 +1,7 @@
-// File: Assistant.kt
-class Assistant(private val assistantName: String) {
+package lab1.task3
+import lab1.task1.Display
+
+class Assistant(val assistantName: String) {
     private val assignedDisplays = mutableListOf<Display>()
 
     fun assignDisplay(display: Display) {
@@ -17,13 +19,19 @@ class Assistant(private val assistantName: String) {
     }
 }
 
-// Main for Task 3
 fun main() {
-    val assistant = Assistant("Display Assistant")
+    val assistant = Assistant("Assistant Johny")
     val display1 = Display(1920, 1080, 401f, "Display1")
     val display2 = Display(2560, 1440, 530f, "Display2")
+    val display3 = Display(3840, 2160, 300f, "Display3")
 
     assistant.assignDisplay(display1)
     assistant.assignDisplay(display2)
+    assistant.assignDisplay(display3)
+
     assistant.assist()
+}
+
+fun runTask() {
+    main()
 }
